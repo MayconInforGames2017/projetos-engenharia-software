a17715b8a421390a9bbbe4c4954c69fc4811cd33
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-04-03 13:30:05 -0300

    criando a classe carinhaservlet

---
+++ Programacao 3/servlet-aula1/src/main/java/com/example/demo/servlet/CarinhaServlet.java
@@ -0,0 +1,11 @@
+package com.example.demo.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class CarinhaServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	
+	
+}
