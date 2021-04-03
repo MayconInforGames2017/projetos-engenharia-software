eeab2dc9dbeae8bfb0f2c57e0a8f38890d40d9af
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-04-03 18:45:56 -0300

    criando a classe local

---
+++ Programacao 3/verdinhas/src/main/java/br/upe/verdinhas/model/Local.java
@@ -0,0 +1,11 @@
+package br.upe.verdinhas.model;
+
+public class Local {
+	
+	private Long id;
+	private String nomeLocal;
+	private String descricao;
+	private boolean cobertura;
+	private String umidade;
+
+}
