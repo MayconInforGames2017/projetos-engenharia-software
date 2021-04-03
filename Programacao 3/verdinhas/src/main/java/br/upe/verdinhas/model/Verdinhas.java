b1ac984ab9f1c12395e22571b96fc08953766974
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-04-03 18:43:01 -0300

    criando a classe verdinhas

---
+++ Programacao 3/verdinhas/src/main/java/br/upe/verdinhas/model/Verdinhas.java
@@ -0,0 +1,13 @@
+package br.upe.verdinhas.model;
+
+import java.time.LocalDate;
+
+public class Verdinhas {
+
+	private Long id;
+	private String especie;
+	private String genero;
+	private String nomePopular;
+	private LocalDate dataAquisicao;
+	
+}
