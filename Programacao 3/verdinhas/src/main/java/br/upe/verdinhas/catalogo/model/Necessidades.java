1b7b58d2aa83c3cd03d556c6d3e80a6c161cabe4
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-04-16 21:28:19 -0300

    imprementando os atributos na classe Necessidades e mapeando o relacionamento

--- Programacao 3/verdinhas/src/main/java/br/upe/verdinhas/catalogo/model/Necessidades.java
+++ Programacao 3/verdinhas/src/main/java/br/upe/verdinhas/catalogo/model/Necessidades.java
@@ -2,7 +2,30 @@ package br.upe.verdinhas.catalogo.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import br.upe.verdinhas.catalogo.enums.TipoVaso;
+
+@Entity
 public class Necessidades implements Serializable {
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id_necessidades")
+	private Long id;
+
+	@OneToOne(mappedBy = "necessidades")
+	private Verdinhas verdinhas;
+
+	@Enumerated(EnumType.STRING)
+	private TipoVaso tipoVaso;
 
 }
